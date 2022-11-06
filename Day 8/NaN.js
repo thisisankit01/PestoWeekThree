855537942b60cc5a964faa1dc8b9fe5693e5c093
Ankit Pandey <ankitechzz@gmail.com>
2022-11-06 22:14:55 +0530

    today i learnt about how NaN works and what is its use

---
+++ Day 8/NaN.js
@@ -0,0 +1,24 @@
+let a = Number('abc');
+console.log(a);
+//NaN
+
+//for undefined
+let b = Number(undefined);
+console.log(b);
+
+//for number + letters
+let c = Number(12 + 'acc');
+console.log(c);
+//NaN
+
+//it is not equal to itself
+let d = Number(undefined);
+console.log(c);
+console.log(c===c);
+//false
+
+//index of NaN
+console.log([1,2,NaN,4].indexOf(NaN)); //finding the index of NaN.
+//its -1.
+
+console.log([1,2,NaN,4].indexOf(2));
