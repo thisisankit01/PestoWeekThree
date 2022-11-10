9a6b8a3c2e77841ff317b1ddb35b072e9cf53131
Ankit Pandey <ankitechzz@gmail.com>
2022-11-10 10:24:23 +0530

    added the ouputs for iterations

--- Day 10/iterations.js
+++ Day 10/iterations.js
@@ -21,12 +21,12 @@ const obj1 = {
   [Symbol()] : 'Hello'
 }
 
-Object.setPrototypeOf(obj1, parent);
+Object.setPrototypeOf(obj1, parent); //setting parent as parent of obj 1 suing sertPrototypeof method.
 
 console.log(obj1.name);
 
-for(let key in obj1){
-  console.log(key, obj1[key]);
+for(let key in obj1){ //using for in loop for  iterating in objects(obj1).
+  console.log(key, obj1[key]); //printing key and value in iteration.
 }
 // Ankit
 // dob 27 Feb
@@ -37,14 +37,46 @@ for(let key in obj1){
 
 const keys = Object.keys(obj);
 const values = Object.values(obj);
-const entries = Object.entries(obj);
+const entries = Object.entries(obj); //entries are entry of an entry
 
-// this method prints properties of current object only, not their parent
-keys.forEach((key)=> console.log(key));
-values.forEach((value)=> console.log(value));
-entries.forEach((pairs)=> console.log(pairs));
+// foreach prints properties of current object only, not their parent.
+keys.forEach((key)=> console.log(key)); // using for each to print all keys.
+values.forEach((value)=> console.log(value)); // usinfg for each tom print all values.
+entries.forEach((pairs)=> console.log(pairs));// used for each to print all pairs of keys and values.
 
-//3. Reflect.ownKeys( )
-console.log(Reflect.ownKeys(obj));
+//outpurt
+// dob
+// age
+// 27 Feb
+// 21
+// [ 'dob', '27 Feb' ]
+// [ 'age', '21' ]
+
+entries.forEach((pairs) =>{
+  const[key,val] = pairs;
+  console.log(key,val);
+});  
+
+//0 { name: 'Ankit' }
+//1 { surname: 'Pandey' }
+
+
+//3. Reflect.ownKeys( ) - popular and powerful method
+console.log(Reflect.ownKeys(obj)); //reflets all keys inlcluding symbols
 console.log(Reflect.ownKeys(Object.prototype));
 
+//output
+
+// [ 'dob', 'age', Symbol() ]
+// [ 'constructor',
+//   '__defineGetter__',
+//   '__defineSetter__',
+//   'hasOwnProperty',
+//   '__lookupGetter__',
+//   '__lookupSetter__',
+//   'isPrototypeOf',
+//   'propertyIsEnumerable',
+//   'toString',
+//   'valueOf',
+//   '__proto__',
+//   'toLocaleString' ]
